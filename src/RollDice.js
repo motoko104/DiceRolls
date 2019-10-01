b010ba9b99ce2b7b463ed8c90b2f675224fc0d74
motoko104 <motoko104@yahoo.com>
2019-09-30 23:58:34 -0500

    adding animation

--- src/RollDice.js
+++ src/RollDice.js
@@ -10,7 +10,8 @@ class RollDice extends Component{
         super(props);
         this.state = {
             die1: 'one',
-            die2: 'one'
+            die2: 'one',
+            rolling: false
         }
         this.roll = this.roll.bind(this);
     }
@@ -21,17 +22,24 @@ class RollDice extends Component{
             const newDie2 = this.props.sides[
                 Math.floor(Math.random() * this.props.sides.length)];
         // set state with new rolls
-        this.setState({die1: newDie1, die2: newDie2});
+        this.setState({die1: newDie1, die2: newDie2, rolling: true});
+
+        //wait one second, then set rolling to falce
+        setTimeout(() => {
+            this.setState({rolling: false});
+        }, 1000);
     }
 
     render(){
         return(
                 <div className="RollDice">
                     <div className="RollDice-container">
-                        <Die face={this.state.die1}/>
-                        <Die face={this.state.die2}/> 
+                        <Die face={this.state.die1} rolling={this.state.rolling}/>
+                        <Die face={this.state.die2} rolling={this.state.rolling}/> 
                     </div>
-                    <button onClick={this.roll}>Roll Dice</button>
+                    <button onClick={this.roll} disabled= {this.state.rolling}>
+                    {this.state.rolling? 'Rolling...': "Roll Dice!"}
+                    </button>
                 </div>
          );
     }
